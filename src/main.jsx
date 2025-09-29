1026571b273d4399439aa8a2c356209902d760e7
Henil <henil247138@gmail.com>
2025-09-30 01:00:53 +0530

    fix error

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import Stair from './components/common/stair.jsx'
+import Stair from './components/common/Stair.jsx'
 import NavContex from './context/NavContext.jsx'
 
 createRoot(document.getElementById('root')).render(
