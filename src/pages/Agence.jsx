e019bbe6f51f3c63ae1c235cd4002cb5db10b045
Henil <henil247138@gmail.com>
2025-09-30 17:42:50 +0530

    Change in agence page for mobile page

--- src/pages/Agence.jsx
+++ src/pages/Agence.jsx
@@ -1,7 +1,7 @@
 import { useGSAP } from '@gsap/react'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
-import React, { useRef } from 'react'
+import React, { useRef, useEffect } from 'react'
 
 // images
 import img1 from '../assets/img/agence/1.png'
@@ -26,42 +26,56 @@ const Agence = () => {
   const imageArray = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14]
 
   gsap.registerPlugin(ScrollTrigger)
-  useGSAP(() => {
-    gsap.to(imageDivRef.current, {
-      scrollTrigger:{
-        trigger:imageDivRef.current,
-        start:"top 26.8%",
-        end: 'top -100%',
-        pin:true,
-        pinSpacing:true,
-        pinReparent: true,
-        pinType: 'transform',
-        scrub:1,
-        anticipatePin:1,
-        invalidateOnRefresh:true,
-        onUpdate: (e) => {
-          const progress = Math.floor(e.progress * (imageArray.length - 1))
-          imageRef.current.src = imageArray[progress]
+  if (window.innerWidth >= 1024) {
+    useGSAP(() => {
+      gsap.to(imageDivRef.current, {
+        scrollTrigger: {
+          trigger: imageDivRef.current,
+          start: "top 26.8%",
+          end: 'top -100%',
+          pin: true,
+          pinSpacing: true,
+          pinReparent: true,
+          pinType: 'transform',
+          scrub: 1,
+          anticipatePin: 1,
+          invalidateOnRefresh: true,
+          onUpdate: (e) => {
+            const progress = Math.floor(e.progress * (imageArray.length - 1))
+            imageRef.current.src = imageArray[progress]
+          }
         }
-      }
+      })
     })
-  })
+  }
+  else {
+    useEffect(() => {
+      let currentIndex = 0
+      const interval = setInterval(() => {
+        if (imageRef.current) {
+          currentIndex = (currentIndex + 1) % imageArray.length
+          imageRef.current.src = imageArray[currentIndex]
+        }
+      }, 500)
+      return () => clearInterval(interval)
+    }, [imageArray])
+  }
   return (
     <div className='bg-black'>
       <div className='section1 py-1 text-white'>
-      <div ref={imageDivRef} className='absolute overflow-hidden lg:h-[18vw] lg:w-[14vw] h-[100px] w-[100px] lg:rounded-3xl rounded-4xl top-50 left-[30vw]'>
-        <img ref={imageRef} className='h-full w-full object-cover' src={img1} alt="" />
-      </div>
-      <div className='relative font-[font2]'>
-        <div className='lg:mt-[52vh] mt-[28vh]'>
-          <h1 className='lg:text-[18vw] text-7xl text-center uppercase leading-[18vw]'>Soixan7e <br />Douze</h1>
+        <div ref={imageDivRef} className='absolute overflow-hidden lg:h-[18vw] lg:w-[14vw] h-[200px] w-[150px] lg:rounded-3xl rounded-4xl lg:top-50 top-40 lg:left-[30vw] left-[20vw] '>
+          <img ref={imageRef} className='h-full w-full object-cover' src={img1} alt="" />
         </div>
-        <div className='pl-[45%] mt-20'>
-          <p className='lg:text-5xl text-3xl'> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.</p>
+        <div className='relative font-[font2]'>
+          <div className='lg:mt-[52vh] mt-[28vh]'>
+            <h1 className='lg:text-[18vw] text-7xl text-center uppercase leading-[18vw]'>Soixan7e <br />Douze</h1>
+          </div>
+          <div className='lg:pl-[45%] p-5 text-right mt-20'>
+            <p className='lg:text-5xl text-xl'> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Notre curiosité nourrit notre créativité. On reste humbles et on dit non aux gros egos, même le vôtre. Une marque est vivante. Elle a des valeurs, une personnalité, une histoire. Si on oublie ça, on peut faire de bons chiffres à court terme, mais on la tue à long terme. C’est pour ça qu’on s’engage à donner de la perspective, pour bâtir des marques influentes.</p>
+          </div>
         </div>
       </div>
-    </div>
-    <div className="section2 h-screen"></div>
+      <div className="section2 h-screen"></div>
     </div>
   )
 }
